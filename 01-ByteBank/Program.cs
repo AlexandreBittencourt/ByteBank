23f1f6d14da00d9110a74e494eea1f04cbefa109
Alexandre <alexandresluzbittencourt@gmail.com>
2022-04-26 17:30:40 -0300

    first commit

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 386;
+            contaDaGabriela.numero = 232323;
+            contaDaGabriela.saldo = 100.00;
+
+            Console.WriteLine("Dados da conta- ");
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: R$" + contaDaGabriela.saldo);
+        }
+    }
+}
---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        ContaCorrente conta = new ContaCorrente();
+
+
+        conta.titular = "Gabriela";
+
+        Console.WriteLine(conta.titular);
+        Console.WriteLine(conta.saldo);
+
+        Console.ReadLine();
+
+    }
+}
